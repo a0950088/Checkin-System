4660253ee2a2f57728f286929082aa99d65d5c0c
a0950088 <a0950088@gmail.com>
2023-04-17 11:11:17 +0800

    clean code

--- checkin.py
+++ checkin.py
@@ -4,7 +4,25 @@ from lxml import html, etree
 from datetime import datetime
 from bs4 import BeautifulSoup as BS
 import parsers
+import log
 import sys
+'''
+1. GET https://portal.ncu.edu.tw/endpoint?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=https%3A%2F%2Fcis.ncu.edu.tw%2FHumanSys%2Flogin&openid.realm=https%3A%2F%2Fcis.ncu.edu.tw&openid.ns.ax=http%3A%2F%2Fopenid.net%2Fsrv%2Fax%2F1.0&openid.ax.mode=fetch_request&openid.ax.type.user_roles=http%3A%2F%2Faxschema.org%2Fuser%2Froles&openid.ax.type.contact_email=http%3A%2F%2Faxschema.org%2Fcontact%2Femail&openid.ax.type.contact_name=http%3A%2F%2Faxschema.org%2Fcontact%2Fname&openid.ax.type.contact_ename=http%3A%2F%2Faxschema.org%2Fcontact%2Fename&openid.ax.type.student_id=http%3A%2F%2Faxschema.org%2Fstudent%2Fid&openid.ax.type.alunmi_leaveSem=http%3A%2F%2Faxschema.org%2Falunmi%2FleaveSem&openid.ax.required=user_roles&openid.ax.if_available=contact_email%2Ccontact_name%2Ccontact_ename%2Cstudent_id%2Calunmi_leaveSem&openid.identity=https%3A%2F%2Fportal.ncu.edu.tw%2Fuser%2F&openid.claimed_id=https%3A%2F%2Fportal.ncu.edu.tw%2Fuser%2F
+
+2. POST https://portal.ncu.edu.tw/login
+
+3. POST https://portal.ncu.edu.tw/leaving
+
+4. GET https://cis.ncu.edu.tw/HumanSys/home
+
+5. GET https://cis.ncu.edu.tw/HumanSys/student/stdSignIn
+
+6. GET https://cis.ncu.edu.tw/HumanSys/student/stdSignIn/create?ParttimeUsuallyId=210885
+
+7. POST https://cis.ncu.edu.tw/HumanSys/student/stdSignIn_detail
+json "isOK"
+
+'''
 ENDPOINT_URL = "https://portal.ncu.edu.tw/endpoint?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=https%3A%2F%2Fcis.ncu.edu.tw%2FHumanSys%2Flogin&openid.realm=https%3A%2F%2Fcis.ncu.edu.tw&openid.ns.ax=http%3A%2F%2Fopenid.net%2Fsrv%2Fax%2F1.0&openid.ax.mode=fetch_request&openid.ax.type.user_roles=http%3A%2F%2Faxschema.org%2Fuser%2Froles&openid.ax.type.contact_email=http%3A%2F%2Faxschema.org%2Fcontact%2Femail&openid.ax.type.contact_name=http%3A%2F%2Faxschema.org%2Fcontact%2Fname&openid.ax.type.contact_ename=http%3A%2F%2Faxschema.org%2Fcontact%2Fename&openid.ax.type.student_id=http%3A%2F%2Faxschema.org%2Fstudent%2Fid&openid.ax.type.alunmi_leaveSem=http%3A%2F%2Faxschema.org%2Falunmi%2FleaveSem&openid.ax.required=user_roles&openid.ax.if_available=contact_email%2Ccontact_name%2Ccontact_ename%2Cstudent_id%2Calunmi_leaveSem&openid.identity=https%3A%2F%2Fportal.ncu.edu.tw%2Fuser%2F&openid.claimed_id=https%3A%2F%2Fportal.ncu.edu.tw%2Fuser%2F"
 NCU_HOST = "https://portal.ncu.edu.tw/login"
 LEAVE_URL = "https://portal.ncu.edu.tw/leaving"
@@ -58,12 +76,6 @@ try:
     # res = session.get(ENDPOINT_URL)
     res, session = HttpMethod(ENDPOINT_URL, 'GET', session)
     cookies = res.cookies
-    # print("-----------------------------")
-    # print(res.status_code)
-    # print(res.cookies)
-    # print(session.cookies)
-    # print("-----------------------------")
-
     print("GET ", ENDPOINT_URL, "Success")
 except Exception as err:
     print("GET ", ENDPOINT_URL, "Failed")
@@ -73,11 +85,6 @@ try:
     # res = session.post(NCU_HOST, data=GetPortalLoginPayload(session), cookies=cookies)
     res, session = HttpMethod(NCU_HOST, 'POST', session, cookies=cookies, data=GetPortalLoginPayload(session))
     cookies = session.cookies
-    # print("-----------------------------")
-    # print(res.status_code)
-    # print(res.cookies)
-    # print(session.cookies)
-    # print("-----------------------------")
     print("POST ", NCU_HOST, "Success")
 except Exception as err:
     print("POST ", NCU_HOST, "Failed")
@@ -86,11 +93,6 @@ except Exception as err:
 try:
     # res = session.post(LEAVE_URL, data=GetPortalLeavingPayload(session), cookies=cookies)
     res, session = HttpMethod(LEAVE_URL, 'POST', session, cookies=cookies, data=GetPortalLeavingPayload(session))
-    # print("-----------------------------")
-    # print(res.status_code)
-    # print(res.cookies)
-    # print(session.cookies)
-    # print("-----------------------------")
     print("POST ", LEAVE_URL, "Success")
 except Exception as err:
     print("POST ", LEAVE_URL, "Failed")
@@ -107,11 +109,6 @@ try:
     page = BS(content, features='lxml')
     username = parsers.ExtractUserName(page)
     print("Login User: ", username)
-    # print("-----------------------------")
-    # print(res.status_code)
-    # print(res.cookies)
-    # print(session.cookies)
-    # print("-----------------------------")
     print("GET ", HOST, "Success")
 except Exception as err:
     print("GET ", HOST, "Failed")
@@ -123,12 +120,6 @@ try:
     cookies = dict(res.cookies)
     cookies['locale'] = session.cookies.get_dict()['locale']
     cookies['BIGipServerpool-cis'] = session.cookies.get_dict()['BIGipServerpool-cis']
-    # print("-----------------------------")
-    # print(res.status_code)
-    # print(res.cookies)
-    # print(session.cookies)
-    # print(content)
-    # print("-----------------------------")
     content = res.content.decode('utf-8')
     page = BS(content, features='lxml')
     ParttimeUsuallyId = parsers.ExtractParttimeUsuallyId(page, projectName)
@@ -151,11 +142,6 @@ try:
     
     content = res.content.decode('utf-8')
     page = BS(content, features='lxml')
-    # print("-----------------------------")
-    # print(res.status_code)
-    # print(res.cookies)
-    # print(session.cookies)
-    # print("-----------------------------")
     idNo = page.find('input', {'id': 'idNo'})
     idNo = idNo.get('value')
     signintime = page.find('div', {'id': 'SigninTime'}).contents
@@ -178,6 +164,7 @@ except Exception as err:
     print("Error: ", err)
 
 if signintime != []:
+    txt = '簽退時間： ' + str(current_time) + '\n'
     signintime_hour = int(signintime[0].split(":")[0])
     if current_time.hour - signintime_hour >= REQUIRE_SIGNIN_TIME:
         try:
@@ -185,48 +172,24 @@ if signintime != []:
             # res = session.post(NCU_POST_CHECKIN_CREATE, cookies=cookies, data=payload)
             res, session = HttpMethod(NCU_POST_CHECKIN_CREATE, 'POST', session, cookies=cookies, data=payload)
             content = res.content.decode('utf-8')
-            # print("-----------------------------")
-            # print(res.status_code)
-            # print(res.cookies)
-            # print(session.cookies)
-            # print(content)
-            # print("-----------------------------")
             print("POST ", NCU_POST_CHECKIN_CREATE, "Success")
+            log.CheckinLog(txt)
         except Exception as err:
             print("POST ", NCU_POST_CHECKIN_CREATE, "Failed")
             print("Error: ", err)
     else:
+        txt = '簽退時間未滿8小時： ' + str(current_time) + '\n'
+        log.CheckinLog(txt)
         print("Not Yet To Signout")
 else:
     print("Signin!")
+    txt = '簽到時間： ' + str(current_time) + '\n'
     try:
         # res = session.post(NCU_POST_CHECKIN_CREATE, cookies=cookies, data=payload)
         res, session = HttpMethod(NCU_POST_CHECKIN_CREATE, 'POST', session, cookies=cookies, data=payload)
         content = res.content.decode('utf-8')
-        # print("-----------------------------")
-        # print(res.status_code)
-        # print(res.cookies)
-        # print(session.cookies)
-        # print(content)
-        # print("-----------------------------")
         print("POST ", NCU_POST_CHECKIN_CREATE, "Success")
+        log.CheckinLog(txt)
     except Exception as err:
         print("POST ", NCU_POST_CHECKIN_CREATE, "Failed")
         print("Error: ", err)
-'''
-1. GET https://portal.ncu.edu.tw/endpoint?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=https%3A%2F%2Fcis.ncu.edu.tw%2FHumanSys%2Flogin&openid.realm=https%3A%2F%2Fcis.ncu.edu.tw&openid.ns.ax=http%3A%2F%2Fopenid.net%2Fsrv%2Fax%2F1.0&openid.ax.mode=fetch_request&openid.ax.type.user_roles=http%3A%2F%2Faxschema.org%2Fuser%2Froles&openid.ax.type.contact_email=http%3A%2F%2Faxschema.org%2Fcontact%2Femail&openid.ax.type.contact_name=http%3A%2F%2Faxschema.org%2Fcontact%2Fname&openid.ax.type.contact_ename=http%3A%2F%2Faxschema.org%2Fcontact%2Fename&openid.ax.type.student_id=http%3A%2F%2Faxschema.org%2Fstudent%2Fid&openid.ax.type.alunmi_leaveSem=http%3A%2F%2Faxschema.org%2Falunmi%2FleaveSem&openid.ax.required=user_roles&openid.ax.if_available=contact_email%2Ccontact_name%2Ccontact_ename%2Cstudent_id%2Calunmi_leaveSem&openid.identity=https%3A%2F%2Fportal.ncu.edu.tw%2Fuser%2F&openid.claimed_id=https%3A%2F%2Fportal.ncu.edu.tw%2Fuser%2F
-
-2. POST https://portal.ncu.edu.tw/login
-
-3. POST https://portal.ncu.edu.tw/leaving
-
-4. GET https://cis.ncu.edu.tw/HumanSys/home
-
-5. GET https://cis.ncu.edu.tw/HumanSys/student/stdSignIn
-
-6. GET https://cis.ncu.edu.tw/HumanSys/student/stdSignIn/create?ParttimeUsuallyId=210885
-
-7. POST https://cis.ncu.edu.tw/HumanSys/student/stdSignIn_detail
-json "isOK"
-
-'''
